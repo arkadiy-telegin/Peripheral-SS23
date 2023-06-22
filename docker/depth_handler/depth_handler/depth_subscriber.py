8519484c2b2a6805cda1f76ef82d8217d8f3c773
juandelos <juandelosrios@hotmail.es>
2023-06-22 18:44:11 +0200

    publisher publishing, depth not correct yet

--- docker/depth_handler/depth_handler/depth_subscriber.py
+++ docker/depth_handler/depth_handler/depth_subscriber.py
@@ -5,7 +5,8 @@ from std_msgs.msg import String, Float64
 from sensor_msgs.msg import Image
 
 import cv2 as cv
-from cv_bridge import CvBridge
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
 bridge = CvBridge()
 
 
@@ -13,23 +14,22 @@ class DepthSubscriber(Node):
 
     def __init__(self):
         super().__init__('minimal_subscriber')
+        self.distance_pub = self.create_publisher(Float64, 'obstacle_distance', 10)
         self.depth_sub = self.create_subscription(
-            String,
+            Image,
             'camera1/depth/image_rect_raw',
             self.get_nearest_point,
             10)
-        self.distance_pub = self.create_publisher(Float64, 'obstacle_distance', 10)
 
     def get_nearest_point(self, msg):
         self.get_logger().info('Image received')
         try:
-            depth_image = cv_bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-
+            depth_image = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+            depth_array = np.array(depth_image, dtype=np.float32)
             warning_msg = Float64()
-
-            min_distance = min(depth_image)
+            min_distance = float(np.min([np.nonzero(depth_array)]))
             warning_msg.data = min_distance
-            self.get_logger().info('Minimal distance', min_distance)
+            self.get_logger().info(f'Min distance: {min_distance}')
             self.distance_pub.publish(warning_msg)
             
                 
