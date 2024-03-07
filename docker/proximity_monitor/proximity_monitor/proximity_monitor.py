33badbf9b4f698d3b53dde258e5dadcb2284fa86
missxa <aliona.kharchenko@gmail.com>
2024-03-07 13:15:45 +0100

    Log throttling

--- docker/proximity_monitor/proximity_monitor/proximity_monitor.py
+++ docker/proximity_monitor/proximity_monitor/proximity_monitor.py
@@ -1,13 +1,17 @@
 import rclpy
 from rclpy.node import Node
 from rcl_interfaces.msg import SetParametersResult
-from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Float32MultiArray, Bool
 from sensor_msgs.msg import Image, CompressedImage
-# from proximity_monitor.msg import ProximityWarning
+
+from rcl_interfaces.srv import SetParameters
+from rclpy.parameter import Parameter
 
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
+
+from enum import Enum
 
 bridge = CvBridge()
 
@@ -16,6 +20,14 @@ bridge = CvBridge()
 # warning: 1 if the distance is less than the threshold
 # proximity: distance to the nearest point (meters)
 # threshold: distance threshold (meters)
+
+class BackCameraMode(Enum):
+    HI_RES = True
+    LOW_RES = False
+
+HI_RES_PROFILE = '424x240x6'
+LOW_RES_PROFILE = '320x240x6'
+    
 
 class ProximityMonitorNode(Node):
     def __init__(self):
@@ -31,11 +43,16 @@ class ProximityMonitorNode(Node):
 
         # Publishers and Subscribers
         self.warning_topic = f'/roboy/pinky/sensing/{self.camera_name}/proximity_warning'
-
         self.warning_publisher = self.create_publisher(Float32MultiArray, self.warning_topic, 10)
-        # self.second_warning_publisher = self.create_publisher(Float32MultiArray, self.second_warning_topic, 10)
 
         self.camera_subscriber = self.create_subscription(Image, f'/{self.camera_name}/depth/image_rect_raw', self.camera_callback, 10)
+        self.parameter_client = self.create_client(SetParameters,
+                                                   f'/{self.camera_name}/set_parameters')
+
+        # Disable mode change for now.
+        # self.camera_mode_subscriber = self.create_subscription(Bool,
+        #                                                        f'/roboy/pinky/sensing/{self.camera_name}/mode',
+        #                                                        self.change_camera_mode, 10)
 
         self.get_logger().info(f'Listening for the camera on /{self.camera_name}/depth/image_rect_raw')
 
@@ -55,23 +72,14 @@ class ProximityMonitorNode(Node):
 
     def camera_callback(self, data):
         self.last_warning, self.last_proximity = self.process_camera_data(data)
-        # self.get_logger().debug(f'Proximity: {self.last_proximity}, Warning: {self.last_warning}')
-        # cv_img = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
-        # compressed_img = cv2.imencode('.jpg', cv_img)[1].tostring()
-        # compressed_msg = CompressedImage()
-        # compressed_msg.header = data.header
-        # compressed_msg.format = "jpeg"
-        # compressed_msg.data = compressed_img
-        # self.get_logger().info(f'Publishing image')
-        # self.image_publisher.publish(compressed_msg)
 
     def publish_warning(self):
         warning_msg = Float32MultiArray()
 
         if self.last_warning:
-            self.get_logger().info(f'Publishing warning: {self.last_proximity}')
+            self.get_logger().info(f'Publishing warning: {self.last_proximity}', throttle_duration_sec=1.0)
         else:
-            self.get_logger().debug(f'Publishing NO warning')
+            self.get_logger().debug(f'Publishing NO warning', throttle_duration_sec=1.0)
 
         warning_msg.data = [
             self.last_warning,
@@ -87,7 +95,7 @@ class ProximityMonitorNode(Node):
                 self.get_logger().info(f'New proximity_warning_threshold set: {self.threshold}')
                 return SetParametersResult(successful=True)
         return SetParametersResult(successful=False)
-    
+
     def nearest_point_distance(self, ros_image):
         try:
             depth_image = bridge.imgmsg_to_cv2(ros_image, desired_encoding='passthrough')
@@ -96,6 +104,16 @@ class ProximityMonitorNode(Node):
             return min_distance * 0.001  # Convert to meters
         except CvBridgeError as e:
             self.get_logger().error(e)
+
+    def change_camera_mode(self, mode_bool: Bool):
+        mode = BackCameraMode(mode_bool.data)
+        self.get_logger().info(f'Changing {self.camera_name} mode to {mode.name}')
+        request = SetParameters.Request()
+        request.parameters = [
+            Parameter(name='rgb_camera.profile', value=LOW_RES_PROFILE if mode == BackCameraMode.LOW_RES else HI_RES_PROFILE).to_parameter_msg(),
+        ]
+        self.parameter_client.call(request)
+
 
 def main(args=None):
     rclpy.init(args=args)
