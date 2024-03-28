23508b0fed89ae78ef201f53a76a2dec1ddde92f
Arkadiy Telegin <arkadiy@akyspace.com>
2024-03-28 19:33:33 +0100

    Rename docker -> src

--- docker/launch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source install/setup.bash
-ros2 launch realsense2_camera multi_camera_launch.py
---
+++ src/launch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source install/setup.bash
+ros2 launch proximity_monitor multi_camera_launch.py
